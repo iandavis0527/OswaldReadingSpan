b80ec95826607e175ec463e4e0f6809fa3279134
Ian Davis <iandavis0527@gmail.com>
2022-03-30 13:50:44 -0400

    Adding display for current version number and changing versioning to use date time instead of major.minor.revision

--- oswald_reading_span/frontend/src/letters/LetterInstructions1.js
+++ oswald_reading_span/frontend/src/letters/LetterInstructions1.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../css/instructions.css";
 import InstructionsPage from "../components/InstructionsPage";
 import { LetterInstructions1ClickedEvent } from "../events/InstructionsEvents";
+import package_data from "../../package.json";
 
 export default class LetterInstructions1 extends React.Component {
   constructor(props) {
@@ -19,6 +20,9 @@ export default class LetterInstructions1 extends React.Component {
         paragraphs={[
           "In this portion of the experiment, you will work on memorizing letters shown on the screen in conjunction with reading and interpreting sentences.",
           "At the start, you will complete some practice sessions to get familiar with how the task works.",
+          <span className={"version-number-block"}>
+            Current Version: {package_data.version}
+          </span>,
         ]}
       />
     );
