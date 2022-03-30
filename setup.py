0fec7fee027a1c617ce7b57911c01f44ceac8561
Ian Davis <iandavis0527@gmail.com>
2022-03-30 13:39:51 -0400

    Bumping version number

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     },
     name="oswald_reading_span",
     packages=find_packages(),
-    version="1.0.19",
+    version="1.0.20",
     description="Cherrypy web server plugin for the oswald shortened reading span task",
     author="Me",
     license="MIT",
